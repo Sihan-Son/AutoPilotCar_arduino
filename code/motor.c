5337f0ba879dd2a1bcfcb40d4a849e5a153352ef
SiHan Son <sihan830@gmail.com>
2017-07-01 22:52:41 +0900

    added pwm code line

--- code/motor.c
+++ code/motor.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-#include <wiringPi.h>
+#include <wiringPi.h> // for using wiringPi, to control GPIO PIN with C
 #include <unistd.h>
-#include <softPwm.h>
+#include <softPwm.h> // for PWM control
 
-#define MOTOR1 1
-#define MOTOR2 0
-#define Speed 4
+#define MOTOR1 1 //GPIO 18
+#define MOTOR2 0 //GPIO 16
+#define Speed 4 //GPIO 23
 
 int main(void)
 {
 	if(wiringPiSetup() ==-1)
-	return 1;
+		return 1;
 	
-	
+	softPwmCreate(Speed,0,100);
 	
 	pinMode(MOTOR1, OUTPUT);
 	pinMode(MOTOR2, OUTPUT);
+	
 
 	for(;;)
 	{
+		softPwmWrite(Speed, 50);
 		digitalWrite(MOTOR1, 1);
 		digitalWrite(MOTOR2, 0);
 		//usleep(50000);
